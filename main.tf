7305d89000794b693ade10d72882176fe216bc04
root <root@ip-172-31-45-201.eu-north-1.compute.internal>
2023-09-15 09:58:49 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-north-1"  # Change this to your desired AWS region
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0b0d560d43e65a601"  # Amazon Linux 2 AMI ID (us-east-1)
+  instance_type = "t3.micro"
+  key_name      = "Keypair"  # Replace with your EC2 key pair name
+
+  tags = {
+    Name = "Terra-ec2"
+  }
+}
+
